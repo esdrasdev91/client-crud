d1e6ebef554d756ec8148275d9aad33565a3b5c8
Esdras Pinheiro <esdras.dev@hotmail.com>
2022-08-17 09:50:28 -0300

    Create ClientService

---
+++ backend/src/main/java/com/esdrasdev/clientcrud/services/ClientService.java
@@ -0,0 +1,17 @@
+package com.esdrasdev.clientcrud.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.esdrasdev.clientcrud.entities.Client;
+
+@Service
+public class ClientService {
+
+	public List<Client> findAll() {
+		return null;
+		
+	}
+	
+}
